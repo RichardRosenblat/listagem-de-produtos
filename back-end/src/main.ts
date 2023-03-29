1b7ed99891e7a448de42dabd3e4d725371010950
Richard Rosenblat <rosenblatr@gmail.com>
2023-03-29 20:07:37 -0300

    refactor backend

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableShutdownHooks();
+  app.enableVersioning();
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Listagem de Produtos')
+    .setDescription('Api para listagem de produtos')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(+process.env.APP_PORT || 3000);
 }
 bootstrap();
