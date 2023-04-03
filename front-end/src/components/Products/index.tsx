720c38a1b8700ac6d2e4d589439e63a2be0ef775
Richard Rosenblat <rosenblatr@gmail.com>
2023-04-03 20:57:33 -0300

    add cards

--- front-end/src/components/Products/index.tsx
+++ front-end/src/components/Products/index.tsx
@@ -7,7 +7,9 @@ import Typography from "@mui/material/Typography";
 import { Box } from "@mui/system";
 import { ProductImagesEnum } from "enums/productImages";
 import { IProduct } from "types/Product";
+import { theme } from "../../styles/theme";
 import { Grid } from "@mui/material";
+import { colors } from "@material-ui/core";
 
 interface props {
 	productsList: IProduct[];
@@ -15,39 +17,80 @@ interface props {
 }
 
 const Products = ({ isLoading, productsList }: props) => {
-	const productTypes: (keyof typeof ProductImagesEnum)[] = [
-		"ELECTRONICS",
-		"CLOTHES",
-		"FOODS",
-		"BOOKS",
-		"OTHER",
-		"ELECTRONICS",
-		"CLOTHES",
-		"FOODS",
-		"BOOKS",
-		"OTHER",
-	];
+	// const productTypes: (keyof typeof ProductImagesEnum)[] = [
+	// 	"ELECTRONICS",
+	// 	"CLOTHES",
+	// 	"FOODS",
+	// 	"BOOKS",
+	// 	"OTHER",
+	// 	"ELECTRONICS",
+	// 	"CLOTHES",
+	// 	"FOODS",
+	// 	"BOOKS",
+	// 	"OTHER",
+	// ];
+
+	const shortenDescription = (description: string) => {
+		if (description.length > 100) {
+			return description.substring(0, 100) + "...";
+		}
+		return description;
+	};
 
 	return (
 		<Grid container spacing={2} sx={{ marginTop: 2 }}>
-			{productTypes.map((type, index) => {
+			{productsList.map((product, index) => {
 				return (
 					<Grid item xs={12} sm={6} md={4} lg={4} xl={3} key={index}>
 						<Box>
 							<Card sx={{ maxWidth: 345 }}>
-								<CardMedia sx={{ height: 140 }} image={ProductImagesEnum[type]} title="green iguana" />
+								<CardMedia
+									sx={{ height: 140 }}
+									image={ProductImagesEnum[product.cathegory]}
+									title={product.name}
+								/>
 								<CardContent>
-									<Typography gutterBottom variant="h5" component="div">
-										{type}
+									<Typography
+										gutterBottom
+										variant="h5"
+										component="div"
+										sx={{
+											color: product.in_stock
+												? theme.palette.text.primary
+												: theme.palette.text.disabled,
+											textDecoration: product.in_stock ? "none" : "line-through",
+										}}
+									>
+										{product.name}
 									</Typography>
-									<Typography variant="body2" color="text.secondary">
-										Lizards are a widespread group of squamate reptiles, with over 6,000 species,
-										ranging across all continents except Antarctica
+									<Typography
+										variant="body2"
+										sx={{
+											color: product.in_stock
+												? theme.palette.text.secondary
+												: theme.palette.text.disabled,
+											textDecoration: product.in_stock ? "none" : "line-through",
+										}}
+									>
+										{shortenDescription(product.description)}
 									</Typography>
 								</CardContent>
 								<CardActions>
-									<Button size="small">Share</Button>
-									<Button size="small">Learn More</Button>
+									<Button size="small">Mais Informação</Button>
+									<Button
+										sx={{
+											textDecoration: product.in_stock ? "none" : "line-through",
+											"&.Mui-disabled": {
+												color: product.in_stock
+													? theme.palette.primary.main
+													: theme.palette.error.light,
+											},
+										}}
+										disabled={!product.in_stock}
+										size="small"
+									>
+										Adicionar ao Carrinho
+									</Button>
 								</CardActions>
 							</Card>
 						</Box>
--- front-end/src/pages/ProductsList/index.tsx
+++ front-end/src/pages/ProductsList/index.tsx
@@ -3,10 +3,8 @@ import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-quer
 import TopBar from "components/TopBar";
 import { getProductList } from "services/getProductList";
 import { IProduct } from "types/Product";
-import { useEffect } from "react";
-import { toast } from "react-hot-toast";
-import { theme } from "styles/theme";
 import Products from "components/Products";
+import ErrorIcon from "@mui/icons-material/Error";
 
 const queryClient = new QueryClient();
 
@@ -20,14 +18,21 @@ const Provider = () => {
 
 const ProductList = () => {
 	const { isLoading, isError, data } = useQuery<IProduct[]>(["productsList"], getProductList, {});
-
 	return (
 		<Container sx={{ width: "100%" }}>
 			<TopBar />
 			{isError ? (
-				<Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh" }}>
-					<Typography variant="h3">
-						<b style={{ color: theme.palette.error.light }}> Erro </b>ao carregar a lista de produtos
+				<Box
+					sx={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "space-around",
+						height: "100vh",
+						width: "100vh",
+					}}
+				>
+					<Typography variant="h3" fontWeight="bold" sx={{ width: "100vh" }}>
+						Erro ao carregar a lista de produtos. <br /> Tente novamente mais tarde.
 					</Typography>
 				</Box>
 			) : (
